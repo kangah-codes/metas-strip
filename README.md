7b81e3513519c0725cf763cb4b7e07ca97ec230c
Joshua Akangah <akangah89@hmail.com>
2020-06-30 04:52:08 +0000

    Final build 0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # metas-strip
 Strip metadata from images
+
+This is a simple GUI based program which is used to delete all metadata contained in an image file
+As of now, it will only work on Linux (Ubuntu) based systems because of some dependencies only in linux
+
+Loading an image
+![LOAD](read.png)
+
+You can see all the metadata which has been read from the image, like phone model, date, etc.
+
+Deleting metadata
+![DELETE](delete.png)
+
+After
+![AFTER](after.png)
+
+After wiping, the metadata no longer exists
+
+
+Requirements
+guizero
+tkinter
+PIL
+pyexiv2
+
+
+Steps to install pyexiv2 for Ubuntu users
+`
+sudo apt-get install build-essential
+sudo apt-get install python-all-dev
+sudo apt-get install libexiv2-dev
+sudo apt-get install libboost-python-dev
+pip3 install py3exiv2
+`
--- app/app.py
+++ app/app.py
@@ -2,28 +2,80 @@
 Author = Joshua Akangah
 """
 
+# importing modules
 from tkinter import *
 from tkinter import filedialog
 import guizero
 from PIL import ImageTk, Image
 import os
+from pyexiv2 import Image as Meta
 
+# setting global varialbes
 input_box = None
+thumbnail = None
+listbox = None
+return_box = None
+
+# creating app window and box widget
 app = guizero.App(title="Metas-Strip")
+buttons_box = guizero.Box(app, width="fill", align="bottom")
 
-def open_image(filename):
-	global input_box
-	a = '/home/pygod/Desktop/favicon.png'
-	img = Image.open(a)
-	message = guizero.Text(app, text="")
-	thumbnail = guizero.Picture(app, image=img, width=100, height=100)
-	message = guizero.Text(app, text="")
-	listbox = guizero.ListBox(app, items=["Beef", "Chicken", "Fish", "Vegetarian"], width="70")
-
-
-
+# about function
 def about():
 	pass
+
+# function to clear screen
+def clear_all():
+	global return_box
+
+	# destroy return box and all items in it
+	return_box.destroy()
+
+# fuction to clear metadata if item
+def clear_meta(item):
+	# ask for user choice before deleting metdata
+	choice = guizero.yesno("Confirm", "Delete metadata?")
+	if choice:
+		guizero.info("Delete", "Deleting metadata")
+		item.clear_exif()
+		item.close()
+		clear_all()
+	else:
+		app.error("Close", "Not deleted")
+
+# function to open image from path
+def open_image():
+	global input_box
+	global return_box
+	metadata = None
+	return_box = guizero.Box(app, width="fill")
+	try:
+		# using PIl to open image
+		img = Image.open(input_box.value)
+		# opening pyexiv2 instance from image
+		meta = Meta(input_box.value)
+		# opening the instance like this so that we don't close it after reading metadata so object is still in memory, although this can cause a memory leak
+
+		# with Meta(input_box.value) as meta: -> using this method will delete the object and we will no longer have access to it in the clear meta function
+		metadata = meta.read_exif()
+		message = guizero.Text(return_box, text="")
+		thumbnail = guizero.Picture(return_box, image=img, width=100, height=100)
+		message = guizero.Text(return_box, text="")
+		if not metadata:
+			# picture has no metadata
+			listbox = guizero.ListBox(return_box, items=["No metadata was extracted"], width="fill", height=50)
+		else:
+			listbox = guizero.ListBox(return_box, items=[], width="fill")
+			cancel = guizero.PushButton(buttons_box, text="Strip Metadata", align="right", command=clear_meta, args=[meta])
+			for _ in metadata.items():
+				# add each item in metadata to listbox
+				listbox.append(f"{_[0]}:     {_[1]}")
+	except FileNotFoundError:
+		message = guizero.Text(return_box, text="")
+		thumbnail = guizero.Picture(return_box, image=f'error.jpeg', width=200, height=200)
+		message = guizero.Text(return_box, text="")
+		message = guizero.Text(return_box, text="Image not found")
+
 
 
 
@@ -34,9 +86,11 @@ menubar = guizero.MenuBar(app,
 
 message = guizero.Text(app, text="")
 message = guizero.Text(app, text="Metas-Strip")
+message = guizero.Text(app, text="Enter image path")
 message = guizero.Text(app, text="")
 input_box = guizero.TextBox(app, width="70")
 message = guizero.Text(app, text="")
-button = guizero.PushButton(app, command=open_image, args=[input_box.value])
+button = guizero.PushButton(app, command=open_image, text="Open Image")
+cancel = guizero.PushButton(buttons_box, text="Clear", align="right", command=clear_all)
 
 app.display()
